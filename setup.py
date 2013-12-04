207cee1dda6fc3a22d9b6216b0e1139a89286488
Wouter Bolsterlee <uws@xs4all.nl>
2013-12-04 14:34:46 +0100

    Add setup script

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='mysql-latin1-codec',
+    version="1.0",
+    py_modules=['mysql_latin1_codec'],
+    author="Wouter Bolsterlee",
+    author_email="uws@xs4all.nl",
+    url="https://github.com/wbolster/mysql-latin1-codec",
+)
